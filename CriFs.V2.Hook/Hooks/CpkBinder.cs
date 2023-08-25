7b861f46bdf025ce58ce29785627bf56cf801e6f
sewer56lol <sewer56lol@googlemail.com>
2023-08-25 02:40:27 +0100

    Fixed: BindFiles Bug in x86 CRI MIddleware Libraries from before pre-2016

--- CriFs.V2.Hook/Hooks/CpkBinder.cs
+++ CriFs.V2.Hook/Hooks/CpkBinder.cs
@@ -9,6 +9,7 @@ using Reloaded.Hooks.Definitions;
 using Reloaded.Memory;
 using Reloaded.Memory.Interfaces;
 using Reloaded.Memory.Sigscan.Definitions;
+using Reloaded.Memory.Sigscan.Definitions.Structs;
 using Reloaded.Memory.Structs;
 using static CriFs.V2.Hook.CRI.CpkBinderPointers;
 using static CriFs.V2.Hook.CRI.CRI;
@@ -84,17 +85,57 @@ public static unsafe class CpkBinder
         var bindFiles = Pointers.CriFsBinder_BindFiles;
         if (IntPtr.Size == 4 && RuntimeInformation.ProcessArchitecture == Architecture.X86)
         {
-            var scanner = scannerFactory.CreateScanner((byte*)bindFiles, 100);
-            var ofs = scanner.FindPattern("6A 01 FF");
-            if (!ofs.Found) 
+            // Try patch `push 1` -> `push -1`
+            var scanner = scannerFactory.CreateScanner((byte*)bindFiles, 34);
+            var ofs = scanner.FindPattern("6A 01");
+            if (ofs.Found)
+            {
+                CriPatchX86BindFilesFileLimit(bindFiles, ofs);
                 return;
+            }
+            
+            // Try patch `1` when it's optimized to be passed via register.
 
-            logger.Info("Patching BindFiles' 1 file limit on x86");
-            Span<byte> newBytes = stackalloc byte[] { 0x6A, 0xFF };
-            Memory.Instance.SafeWrite((nuint)(bindFiles + ofs.Offset), newBytes);
+            // We will check for eax, ecx and edx because these are caller saved; 
+            // we will assume an optimizing compiler would not emit a register pass for
+            // a callee saved register as that would be less efficient than passing by stack,
+            // which is covered by above case.
+            
+            // `40` = inc eax -> `48` = dec eax
+            // `41` = inc ecx -> `49` = dec ecx
+            // `42` = inc edx -> `4A` = dec edx
+
+            // `31 C0` or `33 C0` = xor eax, eax
+            // `31 C9` or `33 C9` = xor ecx, ecx
+            // `31 D2` or `33 D2` = xor edx, edx
+            
+            // We need to check for the 'xor' instruction first, then for presence of associated inc instruction
+            // Changing from inc to dec, will underflow and result in a -1 value
+            void TryPatchIncrement(string sig1, string sig2, string incSig, byte decValue, string message)
+            {
+                if (!scanner.TryFindEitherPattern(sig1, sig2, 0, out var localRes)) 
+                    return;
+
+                if (scanner.TryFindPattern(incSig, localRes.Offset, out var res))
+                {
+                    Memory.Instance.SafeWrite((nuint)((byte*)bindFiles + res.Offset), new Span<byte>(&decValue, 1));
+                    _logger.Info(message);
+                }
+            }
+            
+            TryPatchIncrement("31 C0", "33 C0", "40", 0x48, "Patched inc eax -> dec eax");
+            TryPatchIncrement("31 C9", "33 C9", "41", 0x49, "Patched inc ecx -> dec ecx");
+            TryPatchIncrement("31 D2", "33 D2", "42", 0x4A, "Patched inc edx -> dec edx");
         }
     }
-    
+
+    private static void CriPatchX86BindFilesFileLimit(long bindFiles, PatternScanResult ofs)
+    {
+        _logger.Info("Patching BindFiles' 1 file limit on x86");
+        Span<byte> newBytes = stackalloc byte[] { 0x6A, 0xFF };
+        Memory.Instance.SafeWrite((nuint)(bindFiles + ofs.Offset), newBytes);
+    }
+
     #region Init
 
     private static CriError FinalizeLibraryImpl()
@@ -407,7 +448,9 @@ public static unsafe class CpkBinder
             return err;
         }
     }
-    
+
+    #endregion
+
     private static nint CriFsBinderFindImpl(nint bndrhn, nint path, void* finfo, bool* exist)
     {
         if (path == 0)
@@ -428,7 +471,6 @@ public static unsafe class CpkBinder
         Marshal.FreeHGlobal(tempStr);
         return err;
     }
-    #endregion
 }
 
 internal readonly struct CpkBinding : IDisposable
---
+++ CriFs.V2.Hook/Utilities/ScannerExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Reloaded.Memory.Sigscan.Definitions;
+using Reloaded.Memory.Sigscan.Definitions.Structs;
+
+namespace CriFs.V2.Hook.Utilities;
+
+/// <summary>
+/// Extensions for the <see cref="IScanner"/> class.
+/// </summary>
+internal static class ScannerExtensions
+{
+    internal static bool TryFindPattern(this IScanner scan, string pattern, int offset, out PatternScanResult res)
+    {
+        res = scan.FindPattern(pattern, offset);
+        return res.Found;
+    }
+    
+    internal static bool TryFindEitherPattern(this IScanner scan, string patternA, string patternB, int offset, out PatternScanResult res)
+    {
+        res = scan.FindPattern(patternA, offset);
+        if (res.Found)
+            return true;
+        
+        res = scan.FindPattern(patternB, offset);
+        return res.Found;
+    }
+}
